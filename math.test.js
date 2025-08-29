ab1db3cd111d970358cc7a3d5a7df88c9d8a87b7
Facundo Puig <f.puig@itecriocuarto.org.ar>
2025-08-29 15:54:48 -0300

    Se agregaron todos los archivos necesarios

--- math.test.js
+++ math.test.js
@@ -1,3 +1,6 @@
 const { suma, resta } = require('./math');
 
-console.assert(suma(2, 3) === 5, '
+console.assert(suma(2, 3) === 5, 'Test fallido: 2 + 3 !== 5');
+console.assert(resta(5, 2) === 3, 'Test fallido: 5 - 2 !== 3');
+
+console.log('Todos los tests de math pasaron!');
